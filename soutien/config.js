144d6e3d8d20d11d6fd2e0571167d032f3f1d230
kevinlabens-del <kevin.labens@gmail.com>
2026-08-28 01:30:50 +0200

    feat: connect SOUTIEN frontend to locked API

--- soutien/config.js
+++ soutien/config.js
@@ -1,5 +1,5 @@
 window.CR3ATIX_SOUTIEN_CONFIG = Object.freeze({
-  apiBase: '',
+  apiBase: 'https://cr3atix-soutien-api.creatixprojet.workers.dev',
   mapApiUrl: 'https://gwqojqwcbwoulxrctaqz.supabase.co/functions/v1/cr3atix-admin',
   mapFallbackUrl: '../projects.json',
   publicBaseUrl: 'https://kevinlabens-del.github.io/creatix-project/soutien/',
